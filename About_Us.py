bd09ec367abd95603413be9735caa8c67c699746
Ali Amini <96921261+AliAmini93@users.noreply.github.com>
2022-07-26 11:50:32 +0430

    Update About_Us.py

--- About_Us.py
+++ About_Us.py
@@ -2,16 +2,18 @@
 
 # Form implementation generated from reading ui file 'About_Us.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_AboutUs(object):
     def setupUi(self, AboutUs):
         AboutUs.setObjectName("AboutUs")
-        AboutUs.resize(274, 295)
+        AboutUs.resize(300, 180)
         self.horizontalLayout = QtWidgets.QHBoxLayout(AboutUs)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.textBrowser_About_us = QtWidgets.QTextBrowser(AboutUs)
@@ -23,17 +25,16 @@ class Ui_AboutUs(object):
 
     def retranslateUi(self, AboutUs):
         _translate = QtCore.QCoreApplication.translate
-        AboutUs.setWindowTitle(_translate("AboutUs", "About us"))
+        AboutUs.setWindowTitle(_translate("AboutUs", "About Us"))
         self.textBrowser_About_us.setHtml(_translate("AboutUs", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">This application has been provided by Ali Amini, expert in the statistics group of the National Post Company of Iran.</span></p>\n"
-"<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Contact info:</span></p>\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">ali_amini@post.ir</span></p>\n"
-"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">+989100014392</span></p>\n"
-"<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\"font-family:'Times New Roman'; font-size:13pt;\">This application has been provided by Ali Amini in the statistics and data analysis group of the National Post Company of Iran.</span></p>\n"
+"<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:1pt;\"><br /></p>\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\"font-family:'Times New Roman'; font-size:13pt; font-weight:2000;\">Contact info:</span></p>\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\"font-family:'Times New Roman'; font-size:13pt;\">aliamini09@yahoo.com</span></p>\n"
+"<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\"font-family:'Times New Roman'; font-size:13pt;\">+989100014392</span></p></body></html>"))
 
 
 if __name__ == "__main__":
@@ -44,4 +45,3 @@ if __name__ == "__main__":
     ui.setupUi(AboutUs)
     AboutUs.show()
     sys.exit(app.exec_())
-
